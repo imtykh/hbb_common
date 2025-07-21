0ed80bb4e2a4a84d54d0553ae2b6dcf0a635981d
1mt <youkh@lichzd.com>
2025-07-21 15:03:13 +0800

    修改默认的服务器和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["58.34.175.146:26116"];
+pub const RS_PUB_KEY: &str = "CsAS8FlV8A8tHcbIjYBxTiurBZ5bbzh9GN6RmgSDHHw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
